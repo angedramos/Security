15e3097170e526113b5ebab8902905776ab0d42d
Ángel Eduardo Ramos Mena <angedramos@gmail.com>
2021-09-12 23:26:26 -0500

    Uploading Security Files

---
+++ XSS/Protection Xss/AddProductSecure.php
@@ -0,0 +1,41 @@
+<?php
+require_once 'configSecure.php';
+$result=false;
+
+session_start();
+
+    $getDescription=$_GET['description'];
+    htmlentities($getDescription, ENT_QUOTES, 'UTF-8');
+
+    if (!empty($_POST)){
+        $title = $_POST['title'];
+        $description = $_POST['description'];
+        // Validation
+        $sql = "INSERT INTO modernproducts(title, description) VALUES (:title, :description)";
+        $query = $pdo->prepare($sql);
+        
+        $result = $query->execute([ 'title'=> $title,  'description' => $description]);   
+    }      
+
+ ?>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">  
+
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <form action="AddProductSecure.php" method="post">
+        <section class="product-register">
+            <h1>Add Product</h1>
+            <a href="AllProductsSecure.php">All Registered Products</a>
+            <br>
+            <input type="text" class="controls" autocomplete="off" required name="title" name="title" id="title" placeholder="Enter Title">
+            <br>
+            <textarea class="controls" required name="description" autocomplete="off" id="description" rows="3" placeholder="Enter Description" name="description"></textarea>
+            <input class="botons" type="submit" name="button" value="Save">
+        </form>
+        </section>
+    </body>
+</html>
