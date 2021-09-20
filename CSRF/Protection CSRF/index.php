d3aa95fb157a92d8c6f3e0b616d2e935580fffb9
Angel <angedramos@gmail.com>
2021-09-19 19:26:37 -0500

    Uploading SQLInjection Files

---
+++ CSRF/Protection CSRF/index.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+    <title>Products</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+<style>
+    body {
+      background-position: inherit;
+      background-image: url('fondo3.png');
+    }
+    </style>
+<body>
+    <div class="container">
+        <section class="product-register">
+            <h1>Products</h1>
+            <p>Add a product by clicking <a href="AddProduct.php"><b>here</b></a></p>
+        </section>
+    </div>
+</body>
+</html>
---
+++ SQL Injection/Protection SQL Injection/AllProducts.php
@@ -0,0 +1,43 @@
+<?php
+require_once 'config.php';
+    $queryResult = $pdo->query("SELECT * FROM oldproducts");  
+    
+    // while($row = $queryResult->fetch(PDO::FETCH_ASSOC)){
+    //     var_dump($row);
+    // }
+?>
+<html>
+<head>
+    <title>List Users</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <meta charset="UTF-8">
+</head>
+<style>
+    body {
+      background-image: url('fondo3.png');
+    }
+    </style>
+<body>
+
+    <div class="container">
+
+        <br>
+        <table class="table">
+            <tr>
+                <th>Title</th>
+                <th>Description</th>
+            </tr>
+            <?php
+           while($row = $queryResult->fetch(PDO::FETCH_ASSOC)) {
+            echo '<tr>';
+            echo '<td>' . $row['title'] . '</td>';
+            echo '<td>' . $row['description'] . '</td>';
+            echo '</tr>';
+             }
+             ?>
+        </table>
+        <a href="AddProduct.php">Add Product</a>    
+    </div>
+</body>
+</html>
